e1ebb438ac5e1e2de7b761ec6ba6e6bfa3ed057c
QnHyn <18855953229@163.com>
2019-11-30 21:33:37 +0800

    静态文件上传以及首页面展示

--- flask_news.py
+++ flask_news.py
@@ -1,5 +1,5 @@
 # -- coding: utf-8 --
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 """
 https://pypi.python.org/pypi/Flask-SQLAlchemy
@@ -32,6 +32,27 @@ class News(db.Model):
     # print class对象的时候返回的是数据（新闻标题）、而不是地址。
     def __repr__(self):
         return '<News %r>' % self.title
+
+
+@app.route('/')
+def index():
+    '''新闻首页'''
+    news_list = News.query.all()
+    return render_template('index.html', news_list=news_list)
+
+
+@app.route('/cat/<name>/')
+def cat(name):
+    '''新闻的类别'''
+    news_list = News.query.filter(News.types == name)
+    return render_template('index.html',name=name, news_list=news_list)
+
+
+@app.route('/detail/<int:pk>/')
+def detail(pk):
+    """ 新闻详情页 """
+    new_obj = News.query.get(pk)
+    return render_template('detail.html', new_obj=new_obj)
 
 
 if __name__ == '__main__':
